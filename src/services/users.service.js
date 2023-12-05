68b9d237b823128b9e4c97efe709152425220029
Jaehooni <8362532@gmail.com>
2023-12-06 02:16:56 +0900

    Fix: Change informations in the session

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,16 +1,26 @@
 import { BaseError } from "../../config/error";
 import { status } from "../../config/response.status";
-import { loginResponseDTO, signinResponseDTO } from "../dtos/users.dto";
-import { addUser, getUser, checkUser } from "../models/users.dao";
+import { fridgeMakingResponseDTO, loginResponseDTO, signinResponseDTO } from "../dtos/users.dto";
+import { addUser, getUser, checkUser, addFridge, getFridgeOwnerName } from "../models/users.dao";
+import { getUserID } from "../models/users.sql";
+
+//섹션 여부 확인
+const checkSection = async (req) => {
+    if (req.session.user){
+        return true;
+    }
+
+    return false;
+}
 
 // 회원가입
 export const joinUser = async (body) => {
     const birth = new Date(body.birthYear, body.birthMonth - 1, body.birthDay);
     
     const joinUserData = await addUser({
-        'name': body.name,
         'email': body.email,
         'password': body.password,
+        'name': body.name,
         'gender': body.gender,
         'birth': birth
     });
@@ -28,10 +38,9 @@ export const createSession = async (req) => {
     const userEmail = req.body.email;
     const userPassword = req.body.password;
 
-    console.log(req.session[userEmail]);
+    // console.log(req.session[userEmail]);
 
-    if (req.session[req.body.email]){
-        // console.log("session 유지 중인 유저")
+    if (await checkSection(req)){
         return loginResponseDTO(userEmail);
     }
 
@@ -41,9 +50,13 @@ export const createSession = async (req) => {
     })
 
     if (accessUserData){
-        req.session[userEmail] = userPassword;
+        const userId = await getUserID(userEmail);
+        req.session.user = {
+            id: userId,
+            email: userEmail
+        }
+        console.log(req.session);
         req.session.save();
-        console.log(req.session[userEmail]);
         return loginResponseDTO(userEmail);
     }
 
@@ -53,7 +66,7 @@ export const createSession = async (req) => {
 
 // 로그아웃
 export const deleteSession = async (req) => {
-    if (req.session[req.body.email]){
+    if (await checkSection(req)){
         req.session.destroy();
         return;
     }
@@ -63,5 +76,24 @@ export const deleteSession = async (req) => {
 
 // 냉장고 생성
 export const joinFridge = async (req) => {
-    
+    if (await checkSection(req)){
+        const joinFridgeData = await addFridge({
+            'email': req.body.email,
+            'fridgeName': req.body.fridgeName,
+        }); 
+
+        // console.log(joinFridgeOwnerData);
+        if (joinFridgeData != -1){
+            return fridgeMakingResponseDTO(await getFridgeOwnerName(joinFridgeData), req.body.fridgeName);
+        }
+
+        throw new BaseError(status.FRIDGE_NOT_CREATED);
+    }
+
+    throw new BaseError(status.SESSION_DOES_NOT_EXIST);
+}
+
+//냉장고 삭제
+export const deleteFridge = async (req) => {
+
 }
